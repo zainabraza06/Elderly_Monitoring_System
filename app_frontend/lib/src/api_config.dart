1717b7aa2c5cdd8bb9408ebedd40d98a963f6826
zeeshan-890 <zzabbaskhan830@gmail.com>
2026-04-18 16:38:17 +0500

    fix: update backendBaseUrl to point to the production server

--- app_frontend/lib/src/api_config.dart
+++ app_frontend/lib/src/api_config.dart
@@ -2,7 +2,8 @@ class AppApiConfig {
   AppApiConfig._();
 
   // Change this once to point the app to a different backend host.
-  static const String backendBaseUrl = 'http://10.0.2.2:8000';
+  static const String backendBaseUrl =
+      'https://detection-backend.app.zeeshan-abbas.tech/';
 
   // Shared API prefix used for all backend REST calls.
   static const String apiPrefix = '/api/v1';
